2c7e6552f2a77c6d5fdf62308064438cbfe17392
Matthew <mwanderson04@gmail.com>
2019-07-28 10:08:26 -0600

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Quizzapp
+//
+//  Created by Matthew Anderson on 25/08/2015.
+//  Copyright (c) 2019 Matthew Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    func updateUI() {
+      
+    }
+    
+    func nextQuestion() {
+        
+    }
+    
+    func checkAnswer() {
+        
+    }
+    
+    func startOver() {
+       
+    }
+    
+}
